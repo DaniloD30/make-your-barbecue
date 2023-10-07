894d5ebddc606313f47a1d15dbcf4fe8cc443e7f
Danilo Dias <danilolive30@hotmail.com>
2023-10-07 17:32:28 -0300

    feat: layout detail

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import background from "../../assets/backgroundBarbecue-mid.png";
-import styles from "./Details.module.css";
 import CardDetails from "@/components/details/CardDetails/Card";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -8,6 +5,7 @@ import { useBarbecue } from "@/contexts/BarbecueContext";
 import { useModal } from "@/contexts/ModalContext";
 import Modal from "@/components/barbecueSchedule/Modal";
 import FormAddGuest from "@/components/details/FormAddGuest";
+import { BarbecueLayout } from "@/Layouts/Barbecue";
 
 export default function Details() {
   const { barbecueDetail } = useBarbecue();
@@ -18,23 +16,15 @@ export default function Details() {
       router.push("/barbecue-schedule");
     }
   }, [barbecueDetail, router]);
-  /*
-  TODO: Agenda de churras é um componente, pq se repete na login
-  TODO: Title max width e ellips
-  */
 
   return (
     <>
-      <div className={styles.container}>
-        <Image src={background} alt="backgroundImage" />
-        <div className={styles.containerName}>
-          <div className={styles.name}>Agenda de Churras</div>
-          <CardDetails />
-        </div>
-        <Modal isOpen={isOpen} toggle={toggle}>
-          <FormAddGuest/>
-        </Modal>
-      </div>
+      <BarbecueLayout>
+        <CardDetails />
+      </BarbecueLayout>
+      <Modal isOpen={isOpen} toggle={toggle}>
+        <FormAddGuest />
+      </Modal>
     </>
   );
 }
